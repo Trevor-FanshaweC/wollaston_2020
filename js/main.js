cfd1ceeaf11c074e108dccb741b616ef9495c39f
theMightyT <halvas_trevor@halvas.ca>
2020-07-16 11:59:00 -0400

    adding mobile styles

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
-(function(){
-	"use strict";
-	
+(() => {
+	// "use strict";
 	console.log("fired");
 
-	var button = document.querySelector("#button");
-	var burgerCon = document.querySelector("#burgerCon");
+	let button = document.querySelector("#button"),
+			burgerCon = document.querySelector("#burgerCon");
 
 	function hamburgerMenu() {
 		burgerCon.classList.toggle("slideToggle");
 		button.classList.toggle("expanded");
 	}
 
-	button.addEventListener("click", hamburgerMenu, false);
-		
+	button.addEventListener("click", hamburgerMenu);
 })();
